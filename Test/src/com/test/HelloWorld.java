2a0f44b5099b6974ff2ebedcc682b14f507bcf78
siddhitals <siddhitals@gmail.com>
2020-08-25 15:49:20 -0500

    Commiting to master

--- Test/src/com/test/HelloWorld.java
+++ Test/src/com/test/HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld {
 		System.out.println("Hello World. This is program test for MacBook Pro");
 		System.out.println("This is Github Desktop TEST");
 		System.out.println("This is test");
+		
+		System.out.println("This is testing master branch");
 	}
 
 }
