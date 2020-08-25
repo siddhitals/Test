a636020b38e21abf47260794b56939d571618061
siddhitals <siddhitals@gmail.com>
2020-08-25 15:42:52 -0500

    Testing xyz

--- Test/src/com/test/HelloWorld.java
+++ Test/src/com/test/HelloWorld.java
@@ -7,6 +7,13 @@ public class HelloWorld {
 		System.out.println("Hello World. This is program test for MacBook Pro");
 		System.out.println("This is Github Desktop TEST");
 		System.out.println("This is test");
+		
+		String str="This is cap one Financial";
+		System.out.println(str);
+		int a=10;
+		int b=20;
+		System.out.println("Addition: "+a+b);
+		
 	}
 
 }
